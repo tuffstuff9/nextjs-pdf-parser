ee71039243c59f9eb7646d707e3a90fa235dd0fa
tuffstuff <57072903+tuffstuff9@users.noreply.github.com>
2023-08-05 11:08:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='w-96'>
-        <FileUpload server='/api/upload'/>
+        <FileUpload />
       </div>
     </main>
   );
