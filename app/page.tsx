abc8ab5ae7120f7d2ef563e1b986bb528f2ccbde
tuffstuff <57072903+tuffstuff9@users.noreply.github.com>
2023-08-05 11:07:49 -0400

    Update page.tsx
    added server='/api/upload' to FilePond component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='w-96'>
-        <FileUpload />
+        <FileUpload server='/api/upload'/>
       </div>
     </main>
   );
